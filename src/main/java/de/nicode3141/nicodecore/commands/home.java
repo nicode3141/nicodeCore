6277f660031c1dfb75be26e1581627373e027a84
nicode3141 <nicolas.d.pfeifer@gmail.com>
2023-06-21 18:47:16 +0200

    added home command

--- src/main/java/de/nicode3141/nicodecore/commands/home.java
+++ src/main/java/de/nicode3141/nicodecore/commands/home.java
@@ -1,12 +1,22 @@
 package de.nicode3141.nicodecore.commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class home implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+
+        Location homeLocation = new Location(Bukkit.getWorld("world"), 10 ,10 ,10);
+
+        Player player = (Player) commandSender;
+
+        player.teleport(homeLocation);
+
         return true;
     }
 }
